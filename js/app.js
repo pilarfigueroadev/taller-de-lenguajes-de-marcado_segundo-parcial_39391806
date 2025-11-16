89dffb3c3998a845e3dbf3e164580941556ec2fc
PilÃar Figueroa <mariadelpilar.figueroa@estudiantes.unahur.edu.ar>
2025-11-16 17:00:14 -0300

    add welcome message for admin and refactor js to validate which page is loading

--- js/app.js
+++ js/app.js
@@ -1,28 +1,45 @@
 const $ = (selector) => document.querySelector(selector)
 
-const handleLogin = () => {
-    const USERNAME = 'mari'
-    const PASSWORD = '123'
+const $home = $('.home--page')
+const $admin = $('.admin--dashboard')
+const $colaborate = $('.colaborate--page')
 
-    const username = $('#username').value.trim()
-    const password = $('#password').value.trim()
-    if (username === USERNAME && password === PASSWORD) {
-        window.location.href = `admin.html?user=${username}`
-    } else {
-        alert('Credenciales incorrectas.')
+if ($home) {
+    const handleLogin = () => {
+        const USERNAME = 'mari'
+        const PASSWORD = '123'
+    
+        const username = $('#username').value.trim()
+        const password = $('#password').value.trim()
+        if (username === USERNAME && password === PASSWORD) {
+            window.location.href = `admin.html?user=${username}`
+        } else {
+            alert('Credenciales incorrectas.')
+        }
     }
+
+    $('.popup__open').addEventListener('click', (event) => {
+        event.preventDefault()
+        $('.popup__wrapper').classList.add('popup__wrapper--active')
+    })
+    
+    $('.popup__close').addEventListener('click', () => {
+        $('.popup__wrapper').classList.remove('popup__wrapper--active')
+    })
+    
+    $('.form--login').addEventListener('submit', (event) => {
+        event.preventDefault()
+        handleLogin()
+    })
 }
 
-$('.popup__open').addEventListener('click', (event) => {
-    event.preventDefault()
-    $('.popup__wrapper').classList.add('popup__wrapper--active')
-})
+if ($admin) {
+    const welcomeAdmin = () => {
+        const urlParams = new URLSearchParams(window.location.search)
+        const username = urlParams.get('user')
+        if (!username) return
+        $('.admin__welcome').textContent = `Bienvenido, ${username}!`
+    }
 
-$('.popup__close').addEventListener('click', () => {
-    $('.popup__wrapper').classList.remove('popup__wrapper--active')
-})
-
-$('.form--login').addEventListener('submit', (event) => {
-    event.preventDefault()
-    handleLogin()
-})
+    welcomeAdmin()
+}
